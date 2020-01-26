af633340f08a64a747a980d4ce93f6154ed154e8
anthonyx21 <anthonyx21@gmail.com>
2020-01-25 22:46:34 -0500

    test

--- tests/unit/example.spec.js
+++ tests/unit/example.spec.js
@@ -7,6 +7,6 @@ describe('HelloWorld.vue', () => {
     const wrapper = shallowMount(HelloWorld, {
       propsData: { msg }
     })
-    expect(wrapper.text()).toMatch(msg + 'x')
+    expect(wrapper.text()).toMatch(msg)
   })
 })
